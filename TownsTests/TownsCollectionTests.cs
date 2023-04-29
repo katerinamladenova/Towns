bec9cb41fadb006063b8148bc097180504578a20
katerina.bogdanova <katerina.bogdanova@alphatheta.net>
2023-04-29 23:03:31 +0300

    Add project files.

---
+++ TownsTests/TownsCollectionTests.cs
@@ -0,0 +1,99 @@
+﻿using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Towns;
+
+namespace TownsTests
+{
+    public class TownsCollectionTests
+    {
+        [Test]
+        public void Test_Constructor_EmptyConstructor()
+        {
+            var townCollection = new TownsCollection();
+            Assert.That(townCollection.Towns.Count, Is.EqualTo(0));
+            Assert.That(townCollection.ToString(), Is.Empty);
+        }
+
+        [Test]
+        public void Test_Constructor_SingleTown()
+        {
+            var townCollection = new TownsCollection("Paris");
+            Assert.That(townCollection.Towns.Count, Is.EqualTo(1));
+            Assert.That(townCollection.ToString(), Is.EqualTo("Paris"));
+        }
+        [Test]
+        public void Test_Constructor_TwoTowns()
+        {
+            var townCollection = new TownsCollection("Paris, London");
+            Assert.That(townCollection.Towns.Count, Is.EqualTo(2));
+            Assert.That(townCollection.ToString(), Is.EqualTo("Paris, London"));
+        }
+        [Test]
+        public void Test_Add_SingleTown()
+        {
+            var townCollection = new TownsCollection("Paris, London");
+            townCollection.Add("Sofia");
+            Assert.That(townCollection.Towns.Count, Is.EqualTo(3));
+            Assert.That(townCollection.ToString(), Is.EqualTo("Paris, London, Sofia"));
+        }
+
+        [Test]
+        public void Test_Add_EmptyTown()
+        {
+            var townCollection = new TownsCollection("Paris, London");
+            townCollection.Add("");
+            Assert.That(townCollection.Towns.Count, Is.EqualTo(2));
+            Assert.That(townCollection.ToString(), Is.EqualTo("Paris, London"));
+        }
+
+        [Test]
+        public void Test_Add_DuplicateTown()
+        {
+            var townCollection = new TownsCollection("Paris, London");
+            townCollection.Add("Paris");
+            Assert.That(townCollection.Towns.Count, Is.EqualTo(2));
+            Assert.That(townCollection.ToString(), Is.EqualTo("Paris, London"));
+        }
+
+        [Test]
+        public void Test_RemoveAtIndex_ValidIndex()
+        {
+            var townCollection = new TownsCollection("Paris, London");
+            townCollection.RemoveAt(0);
+            Assert.That(townCollection.Towns.Count, Is.EqualTo(1));
+            Assert.That(townCollection.ToString(), Is.EqualTo("London"));
+        }
+
+        [Test]
+        public void Test_RemoveAtIndex_InvalidIndex()
+        {
+            var townCollection = new TownsCollection("Paris, London");
+            // townCollection.RemoveAt(5);
+            Assert.That(() => townCollection.RemoveAt(5), Throws.InstanceOf<ArgumentException>());
+            // Assert.That(townCollection.ToString(), Is.EqualTo("London"));
+        }
+
+        [Test]
+        public void Test_Reverse_TwoTowns()
+        {
+            var townCollection = new TownsCollection("Paris, London");
+            townCollection.Reverse();
+            Assert.That(townCollection.Towns.Count, Is.EqualTo(2));
+            Assert.That(townCollection.ToString(), Is.EqualTo("London, Paris"));
+        }
+
+        [Test]
+        public void Test_Reverse_SingleTown()
+        {
+            var townCollection = new TownsCollection("");
+            //townCollection.Reverse();
+            Assert.That(() => townCollection.Reverse(), Throws.InstanceOf<InvalidOperationException>());
+        }
+
+    }
+}
+
---
+++ TownsTests/TownsProcessorTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Towns;
+
+namespace TownsTests
+{
+    public class TownsProcessorTests
+    {
+        [Test]
+        public void Test_CreateTowns_verifyCorrectResult()
+        {
+            var townProcessor = new TownsProcessor();
+            var createCommand = "CREATE Dolno Uino, Buichinovchi, Trun";
+
+            townProcessor.ExecuteCommand(createCommand);
+            var printInput = "PRINT";
+
+            var actualResult = townProcessor.ExecuteCommand(printInput);
+
+            Assert.That(actualResult, Is.EqualTo("Towns: Dolno Uino, Buichinovchi, Trun"));
+            Assert.That(townProcessor.Towns.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Test_AddTown_verifyAddedCorrect()
+        {
+            var townProcessor = new TownsProcessor();
+            var createCommand = "CREATE Dolno Uino, Buichinovchi, Trun";
+
+            townProcessor.ExecuteCommand(createCommand);
+            var addTown = "ADD Bagdad";
+            //var printInput = "PRINT";
+
+            var actualResult = townProcessor.ExecuteCommand(addTown);
+
+
+            Assert.That(actualResult, Is.EqualTo("Successfully added: Bagdad"));
+            Assert.That(townProcessor.Towns.Count, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Test_RemoveAtTown_verifyRemovedCorrect()
+        {
+            var townProcessor = new TownsProcessor();
+            var createCommand = "CREATE Dolno Uino, Buichinovchi, Trun";
+
+            townProcessor.ExecuteCommand(createCommand);
+            var removeTown = "REMOVE 1";
+
+            var actualResult = townProcessor.ExecuteCommand(removeTown);
+            //var printInput = "PRINT";
+
+
+            Assert.That(actualResult, Is.EqualTo("Successfully removed from index: 1"));
+            Assert.That(townProcessor.Towns.Count, Is.EqualTo(2));
+        }
+
+        
+        [Test]
+        public void Test_ReverseCollection_verifySuccessfulReversed()
+        {
+            var townProcessor = new TownsProcessor();
+            var createCommand = "CREATE Dolno Uino, Buichinovchi, Trun";
+
+            townProcessor.ExecuteCommand(createCommand);
+            var reverseTowns = "REVERSE";
+
+            var actualResult = townProcessor.ExecuteCommand(reverseTowns);
+
+            Assert.That(actualResult, Is.EqualTo("Collection of towns reversed."));
+            Assert.That(townProcessor.Towns.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Test_ReverseCollectionIncorrectData_returnsInformationMessage()
+        {
+            var townProcessor = new TownsProcessor();
+            var createCommand = "CREATE Sofia";
+
+            townProcessor.ExecuteCommand(createCommand);
+            var reverseTowns = "REVERSE";
+
+           var actualResult = townProcessor.ExecuteCommand(reverseTowns);
+                        
+           //Assert.That(() => townProcessor.ExecuteCommand("REVERSE"), Throws.InstanceOf<InvalidOperationException>());
+           Assert.That(actualResult, Is.EqualTo("Cannot reverse a collection of towns with less than 2 items."));
+        }
+    }
+}
